6b4010e21b803668ee93cf93c39513da1a549b93
MiguelR222 <miguel.ruiz0652@gmail.com>
2025-03-11 18:50:37 -0600

    Add Login View

---
+++ app/src/main/java/com/example/mobile1project/signup/views/SignupView.kt
@@ -0,0 +1,14 @@
+package com.example.mobile1project.signup.views
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignupView(){
+    Column {
+        Text(
+        "Registro"
+    )
+    }
+}
